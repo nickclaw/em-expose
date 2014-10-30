ece963b4ca4618011e320129afafe6a6c8624409
Nicholas Clawson <nickclaw@gmail.com>
2014-10-29 17:57:46 -0700

    Add set function to util.js

---
+++ test/util.set.js
@@ -0,0 +1,35 @@
+var expect = require('chai').expect,
+    set = require('../lib/util.js').set;
+
+describe('util#set', function() {
+
+    it('should set the value of a flat object', function() {
+        var obj = {};
+        set(obj, 'a', 'a');
+
+        expect(obj.a).to.equal('a');
+    });
+
+    it('should create objects that do not exist', function() {
+        var obj = {};
+        set(obj, 'a.a.a', 'a');
+
+        expect(obj.a.a.a).to.equal('a');
+    });
+
+    it('should overwrite undefined and null values', function() {
+        var obj = {a: null};
+        set(obj, 'a.a.a', 'a');
+
+        expect(obj.a.a.a).to.equal('a');
+    });
+
+    it('should not overwrite objects, just add to', function() {
+        var obj = {a: {a: 'a'}};
+        set(obj, 'a.b', 'a');
+
+        expect(obj.a.a).to.equal('a');
+        expect(obj.a.b).to.equal('a');
+    });
+
+});
