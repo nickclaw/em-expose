b598088b2c092f1ec63c3ce39e7709f7e4cb1386
Nicholas Clawson <nickclaw@gmail.com>
2015-01-26 15:44:13 -0800

    Where I'm at

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var util = require('./lib/util.js'),
     Exposer = require('./lib/exposer.js');
 
-module.exports = new Exposer();
+module.exports = new Exposer({});
 module.exports.Exposer = Exposer; // so we can make more than one
 module.exports.util = util;
